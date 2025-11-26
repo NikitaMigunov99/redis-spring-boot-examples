bf7dbfa8de32d65fe147d6092e0c07defbadae05
Nikita <nikita1999.nm@gmail.com>
2025-11-26 13:52:37 +0300

    Return right command

--- src/main/kotlin/com/redis/examples/redis/CommandArgsFactory.kt
+++ src/main/kotlin/com/redis/examples/redis/CommandArgsFactory.kt
@@ -11,7 +11,7 @@ class CommandArgsFactory {
         val args = CommandArgs(ByteArrayCodec.INSTANCE)
             .addKey(key)
             .add(ttl.toString())
-            .add("F")
+            .add("FIELDS")
             .add("1")
             .add(domain)
         return args
