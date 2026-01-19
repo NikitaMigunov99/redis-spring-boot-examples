1f0fc3f90ff963a213cb490fb2d3677d4965d787
Nikita <nikita1999.nm@gmail.com>
2026-01-19 14:26:27 +0300

    Add set and get, increase timeout and TTL

--- src/main/kotlin/com/redis/examples/controller/TestController.kt
+++ src/main/kotlin/com/redis/examples/controller/TestController.kt
@@ -1,8 +1,11 @@
 package com.redis.examples.controller
 
 import com.redis.examples.service.EmailService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 
 @RestController
@@ -16,12 +19,16 @@ class TestController(private val emailService: EmailService) {
 
     @PostMapping("/run")
     fun runTenTasks() {
-        val executor = Executors.newFixedThreadPool(10)
-        repeat(15) { index ->
-            executor.submit {
-                Thread.sleep((100L..500L).random()) // небольшая задержка
-                emailService.process(domains[index])
-            }
-        }
+        CompletableFuture.runAsync { emailService.process("gmail.com") }
+    }
+
+    @PostMapping("/set/{domain}")
+    fun setDomain(@PathVariable domain: String) {
+        emailService.process(domain)
+    }
+
+    @GetMapping("/get/{domain}")
+    fun getByDomain(@PathVariable domain: String) : Int {
+        return emailService.getCounter(domain)
     }
 }
