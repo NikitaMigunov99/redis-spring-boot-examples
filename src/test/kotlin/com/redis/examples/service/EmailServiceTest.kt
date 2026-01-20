06f4d9b3e9f246fdc68fceede5c64dbb337e9630
Nikita <nikita1999.nm@gmail.com>
2026-01-20 10:38:43 +0300

    Add multiple Tasks test

--- src/test/kotlin/com/redis/examples/service/EmailServiceTest.kt
+++ src/test/kotlin/com/redis/examples/service/EmailServiceTest.kt
@@ -3,9 +3,12 @@ package com.redis.examples.service
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.Executors
 
 @SpringBootTest
 class EmailServiceTest {
+
+    private val executor = Executors.newFixedThreadPool(10)
 
     @Autowired
     private lateinit var emailService: EmailService
@@ -22,6 +25,21 @@ class EmailServiceTest {
         Thread.sleep(5000)
         println("Setting value without TTL")
         emailService.setValue("gmail.com", 1000)
+        val value = emailService.getCounter("gmail.com")
+        println("My value $value after setting")
+    }
+
+    @Test
+    fun multipleTasks() {
+        println("multipleTasks Test")
+        for (i in 1..10) {
+            executor.submit {
+                println("Processing counter: $i")
+                emailService.process("gmail.com")
+                println("Finished counter: $i")
+            }
+        }
+        Thread.sleep(5000)
         val value = emailService.getCounter("gmail.com")
         println("My value $value after setting")
     }
